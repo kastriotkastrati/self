44804a1b7b5ce475888a808ad5b179d27cf968a4
kastriotkastrati <45375635+kastriotkastrati@users.noreply.github.com>
2022-09-04 19:54:03 +0200

    create: start-nyclient

---
+++ terminal/start-nyclient.ps1
@@ -0,0 +1,4 @@
+Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange"
+& "venv\nextvenv\scripts\activate"
+Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange\client"
+npm run dev
