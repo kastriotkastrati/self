cbffa0d10518ec70411c9de9dacdb81763594392
kastriotkastrati <45375635+kastriotkastrati@users.noreply.github.com>
2022-09-04 19:54:43 +0200

    Create start-nyflask.ps1

---
+++ terminal/start-nyflask.ps1
@@ -0,0 +1,4 @@
+Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange"
+& "venv\pyvenv\scripts\activate"
+Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange\processing"
+flask run
