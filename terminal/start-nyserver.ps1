e20660f7cab7406b01db5fc1a59b83da76975232
kastriotkastrati <45375635+kastriotkastrati@users.noreply.github.com>
2022-09-04 19:50:23 +0200

    create: doge server start

---
+++ terminal/start-nyserver.ps1
@@ -0,0 +1,4 @@
+Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange"
+& "venv\nodevenv\scripts\activate"
+Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange\server"
+npm run dev
