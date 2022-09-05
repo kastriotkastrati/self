2804df3a1d1577c845e9ebd16ff82e144648d8b6
KastriotKastrati <kastriotrkastrati@gmail.com>
2022-09-05 22:54:20 +0200

    add: start chrome at localhost on nyclient

--- terminal/start-nyclient.ps1
+++ terminal/start-nyclient.ps1
@@ -2,4 +2,5 @@ Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange"
 & "venv\nextvenv\scripts\activate"
 Set-Location -Path "C:\Users\KastriotKastrati\desktop\nydogeexchange\client"
 code .
+Start-Process "http://localhost:3000"
 npm run dev
