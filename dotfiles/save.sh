4d6aab15c4130357f651d117bef338f09b0c0812
Kastriot Kastrati <kastriotrkastrati@gmail.com>
2022-10-01 04:10:02 +0200

    dotfiles: +%d, %B, %y, %HH:mm

--- dotfiles/save.sh
+++ dotfiles/save.sh
@@ -1,9 +1,9 @@
 
 run_save(){
-  parsed_date = $date+"%d, %B, %y, %HH:mm"
+  parsed_date=$date+"%d, %B, %y, %HH:mm"
   cd ~/coding/self
   git add "./dotfiles"
-  git commit -m "dotfiles:"
+  git commit -m "dotfiles: $parsed_date"
   git push
 }
 
