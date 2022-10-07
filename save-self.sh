9f966811173e808383051690efad56b408e780b5
Kastriot Kastrati <kastriotrkastrati@gmail.com>
2022-10-07 22:06:53 +0200

    save: Fri, 07 October, 22, 22:10

--- save-self.sh
+++ save-self.sh
@@ -5,7 +5,7 @@ run_save() {
   cd ~/coding/self
   parsed_date=$(date +"%a, %d %B, %y, %H:%m")
   git add .
-  git commit -m "save: $@ -- $parsed_date"
+  git commit -m "save: $parsed_date"
   git push
 }
 
